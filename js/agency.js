4df4d207a9096c8e8f2e3761fbed49d741fa209a
Kris Zhelyazkov <kris@kriss-air.attlocal.net>
2017-05-08 19:25:39 -0400

    funcioning carousel

--- js/agency.js
+++ js/agency.js
@@ -3,6 +3,7 @@
 
     // Smooth scrolling using jQuery easing
     $('a[href*="#"]:not([href="#"])').click(function() {
+        if( $(this).attr("href")=="#myCarousel") return;
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
